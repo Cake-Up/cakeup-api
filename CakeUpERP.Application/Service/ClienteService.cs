51c4f42998e8c0e3bb77b1e59354d63a8f57163c
Leandro Santos <leandrooliveira574@gmail.com>
2023-06-26 18:34:53 -0300

    CAK-12 Preparando Infra

---
+++ CakeUpERP.Application/Service/ClienteService.cs
@@ -0,0 +1,19 @@
+﻿using CakeUpERP.Application.Interfaces;
+using CakeUpERP.Domain.Interfaces.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeUpERP.Application.Service
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository _repository;
+        public ClienteService(IClienteRepository repository)
+        { 
+            _repository = repository;
+        }
+    }
+}
---
+++ CakeUpERP.Infra.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using CakeUpERP.Domain.Entities;
+using CakeUpERP.Domain.Interfaces.Repositorys;
+using CakeUpERP.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeUpERP.Infra.Data.Repository
+{
+    public class ClienteRepository : RepositoryBase<ClienteEntity>, IClienteRepository
+    {
+        public ClienteRepository(DataContext DbContext) : base(DbContext) { }
+    }
+}
