0ba870aac000ce82299be992a6e03fa4ab87701b
Leandro Santos <leandrooliveira574@gmail.com>
2023-08-29 01:32:32 -0300

    feat: adicionado tabela registro atualizacoes ingredientes

--- CakeUpERP.Domain/Entities/AtualizacoesPrecos.cs
+++ CakeUpERP.Domain/Entities/AtualizacoesPrecos.cs
@@ -8,6 +8,12 @@ namespace CakeUpERP.Domain.Entities
 {
     public class AtualizacoesPrecosEntity: EntityBase
     {
-
+        public DateTime DataAtualizacao { get; set; }
+        public int IdReceita { get; set; }
+        public int IdIngrediente { get; set; }
+        public int QtdProduto { get; set; }
+        public int Preco { get; set; }
+        public IngredienteEntity Ingrediente { get; set; }
+        public ReceitaEntity Receita { get; set; }
     }
 }
